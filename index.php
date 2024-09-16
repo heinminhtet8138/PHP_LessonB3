fd443ca665bfd8e5cb45f1991efe6067dba0b001
heinminhtet8138 <heinminhtet8138@gmail.com>
2024-09-16 21:59:31 +0630

    php lesson

--- index.php
+++ index.php
@@ -1,4 +1,27 @@
 <?php 
     echo "Hello World!";
     echo "<h1>Super Friday.</h1>";
+    $language_type = "Server-side programming language";
+    echo $language_type;
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        echo "<h1>This is paragraph</h1>";
+    ?>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam doloribus beatae id quia. Dolorem, quis? Eius accusamus eos impedit quam fugit obcaecati non blanditiis molestias officia quisquam nemo pariatur magni vel ducimus harum voluptatibus incidunt, laborum voluptatem! Esse tempore, obcaecati at eveniet reiciendis ad veniam laborum, ipsum voluptates adipisci eius.
+    </p>
+
+    <script>
+        document.write('<h1>I am JavaScript!</h1>');
+    </script>
+</body>
+</html>
---
+++ looping.php
@@ -0,0 +1,36 @@
+<?php 
+
+    //for loop
+    $a = 0;
+    $b = 0;
+    for($i = 0; $i < 15; $i++) { //i = 0, 1, 2, 3....,15
+        $a += 10; // a = 10 + 10 + 10
+        $b += 5;  // b = 5 + 5 + 5
+    }
+    echo "<h3>The looping end is a = $a and b = $b.</h3>";
+
+    //while loop
+    $j = 10;
+    $num = 50;
+    while($j < 10) {
+        $num--;
+        $j++;
+    }
+    echo "<h3>The looping end at j = $j and num = $num</h3>";
+
+    //do while loop
+    $k = 10;
+    $n = 50;
+    do {
+        $k++;
+        $n++;
+    }while ($k < 10);
+    echo "<h3>The looping end at k = $k and n = $n</h3>";
+    
+    //For each
+    $laptops = ["Apple","Dell","Hp","Asus","Lenovo"];
+    print_r($laptops);
+    foreach($laptops as $key => $value) {
+        echo "<h3>Key is $key and Value is $value</h3>";
+    }
+?>
